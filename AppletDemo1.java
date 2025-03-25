a7faf6ade1ff0b11d0eef8a3edba1b42cfe63ac2
deepikakethavath8688 <deepikakethavath2@gmail.com>
2025-03-25 13:31:04 +0530

    Add files via upload

---
+++ AppletDemo1.java
@@ -0,0 +1,20 @@
+import java.applet.*;
+import java.awt.*;
+
+/*
+<applet code="AppletDemo1.class" width="300" height="300">
+</applet>
+*/
+
+public class AppletDemo1 extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawString("Hello World!",100,150);
+g.drawLine(150,300,250,300);
+g.drawLine(175,350,175,550);
+g.drawLine(Red,Yellow);
+g.drawRect(200,400,100,150);
+g.drawRect(300,350,200,200);
+} 
+}
